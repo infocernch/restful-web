ae25a8684d0c9d4e1efb01496807d34f61dcc219
woo <wbg030281@gmail.com>
2023-08-12 19:13:46 +0900

    깃허브연동

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
-//s
+
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
